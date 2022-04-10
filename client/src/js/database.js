4ce87c12a012147da0d970aa8b7b5ebd773acea8
eeoerke <eeoerke@gmail.com>
2022-04-09 18:03:20 -0600

    completed logic for the put and getall routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
 		},
 	});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+// Logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+	console.log('update DB');
+	const dB = await openDb('jate', 1);
+	const text = dB.transaction('jate', 'readwrite');
+	const store  = text.objectStore('jate');
+	const request = store.put({value: content});
+	const result = await request;
+	console.log("DB is updated", result)
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+	console.log('get all DB entries');
+	const dB = await openDb('jate', 1);
+	const text = dB.transaction('jate', 'readonly');
+	const store  = text.objectStore('jate');
+	const request = store.getAll();
+	const result = await request;
+	console.log(result)
+	return result;
+};
 
 initdb();
