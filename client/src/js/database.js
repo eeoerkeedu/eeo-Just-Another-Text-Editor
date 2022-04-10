86b5f153ec160d96e2af8d2049dd6552d6e39f5c
eeoerke <eeoerke@gmail.com>
2022-04-09 18:31:00 -0600

    npm build, dist files, bug with install and db read write found

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // Logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 	console.log('update DB');
-	const dB = await openDb('jate', 1);
+	const dB = await openDB('jate', 1);
 	const text = dB.transaction('jate', 'readwrite');
 	const store  = text.objectStore('jate');
 	const request = store.put({value: content});
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
 // Logic for a method that gets all the content from the database
 export const getDb = async () => {
 	console.log('get all DB entries');
-	const dB = await openDb('jate', 1);
+	const dB = await openDB('jate', 1);
 	const text = dB.transaction('jate', 'readonly');
 	const store  = text.objectStore('jate');
 	const request = store.getAll();
